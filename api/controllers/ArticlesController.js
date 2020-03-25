053b1cc33457b9b6a37df7f000ee3b0f2adcfc5d
harshil <sharshil43@yahoo.com>
2020-03-25 17:20:41 +0530

    sails crud demo articles

---
+++ api/controllers/ArticlesController.js
@@ -0,0 +1,66 @@
+/**
+ * ArticlesController
+ *
+ */
+
+module.exports = {
+	list:function(req, res){
+        Articles.find({}).exec(function(err, articles){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('pages/list', {articles:articles});
+        });
+    },
+    add: function(req, res){
+        console.log("add api called")
+        res.view('pages/add');
+    },
+    create:function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.create({title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+    },
+    delete: function(req, res){
+        Articles.destroy({id:req.params.id}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+
+        return false;
+    },
+    edit: function(req, res){
+        Articles.findOne({id:req.params.id}).exec(function(err, article){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.view('pages/edit', {article:article});
+        });
+    },
+    update: function(req, res){
+        var title = req.body.title;
+        var body = req.body.body;
+
+        Articles.update({id: req.params.id},{title:title, body:body}).exec(function(err){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+
+        return false;
+    }
+};
+
