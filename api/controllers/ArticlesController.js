647ba7c44262c596f9b00788286f5f7d6f1fd40a
harshil <sharshil43@yahoo.com>
2020-03-25 17:58:41 +0530

    convert cbs to promises

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -20,45 +20,45 @@ module.exports = {
         var title = req.body.title;
         var body = req.body.body;
 
-        Articles.create({title:title, body:body}).exec(function(err){
-            if(err){
+        Articles.create({title:title, body:body})
+            .then(result => {
+                res.redirect('/articles/list');
+            })
+            .catch(err => {
                 res.send(500, {error: 'Database Error'});
-            }
-
-            res.redirect('/articles/list');
-        });
+            })
     },
     delete: function(req, res){
-        Articles.destroy({id:req.params.id}).exec(function(err){
-            if(err){
+        Articles.destroy({id:req.params.id})
+            .then(result => {
+                res.redirect('/articles/list');
+            })
+            .catch(err => {
                 res.send(500, {error: 'Database Error'});
-            }
-
-            res.redirect('/articles/list');
-        });
+            })
 
         return false;
     },
     edit: function(req, res){
-        Articles.findOne({id:req.params.id}).exec(function(err, article){
-            if(err){
-                res.send(500, {error: 'Database Error'});
-            }
-
-            res.view('pages/edit', {article:article});
-        });
+        Articles.findOne({id:req.params.id})
+            .then(result => {
+                res.redirect('/articles/list');
+            })
+            .catch(err => {
+                res.view('pages/edit', {article:article});
+            })
     },
     update: function(req, res){
         var title = req.body.title;
         var body = req.body.body;
 
-        Articles.update({id: req.params.id},{title:title, body:body}).exec(function(err){
-            if(err){
+        Articles.update({id: req.params.id},{title:title, body:body})
+            .then(result => {
+                res.redirect('/articles/list');
+            })
+            .catch(err => {
                 res.send(500, {error: 'Database Error'});
-            }
-
-            res.redirect('/articles/list');
-        });
+            })
 
         return false;
     }
